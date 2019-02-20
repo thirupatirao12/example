684523db2728387122d6c23479ae2c2ef3b1b79a
thirupatirao12 <44597155+thirupatirao12@users.noreply.github.com>
2019-02-20 13:48:40 +0530

    Add files via upload

---
+++ DemoAddition.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class DemoAddition
+
+{
+public static void main(String[] args)
+
+{  
+int a=20;
+int b=30;
+int c=a+b;
+
+System.out.println("add of the two values"+c);
+}
+
+}
